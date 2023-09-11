830dbff3b9c09cb20c396447813157b2046f1892
Clément OUDOT <clement@oodo.net>
2023-09-11 17:30:06 +0200

    Use RTD theme

--- source/conf.py
+++ source/conf.py
@@ -17,10 +17,11 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'LSC'
+project = 'LDAP Synchronization Connector project'
 copyright = '2023, LSC Team'
 author = 'LSC Team'
 
+release = '1.0'
 
 # -- General configuration ---------------------------------------------------
 
@@ -44,7 +45,8 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
